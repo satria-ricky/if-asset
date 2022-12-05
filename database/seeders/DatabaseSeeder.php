4bc5b8f32308a3d4274bfb59cbe8b8cd1cad4897
satria-ricky <satria.ricky48@gmail.com>
2022-12-05 15:35:35 +0800

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,39 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(4)->create();
         \App\Models\Aset::factory(5)->create();
-        \App\Models\Ruangan::factory(3)->create();
+        // \App\Models\Ruangan::factory(3)->create();
         \App\Models\Sumber::factory(3)->create();
-        \App\Models\Kondisi::factory(3)->create();
+        // \App\Models\Kondisi::factory(3)->create();
+        DB::table('kondisis')->insert([
+            [
+                'nama_kondisi'    => 'Rusak'
+            ],
+            [
+                'nama_kondisi'    => 'Baik'
+            ]
+        ]);
+
+        DB::table('ruangans')->insert([
+            [
+                'nama_ruangan'    => 'Lab 1'
+            ],
+            [
+                'nama_ruangan'    => 'Lab 2'
+            ],
+            [
+                'nama_ruangan'    => 'Lab 3'
+            ],
+            [
+                'nama_ruangan'    => 'Lab 4'
+            ],
+            [
+                'nama_ruangan'    => 'Ruang D3-03'
+            ],
+            [
+                'nama_ruangan'    => 'Ruang D4-03'
+            ]
+        ]);
+
+        
     }
 }
